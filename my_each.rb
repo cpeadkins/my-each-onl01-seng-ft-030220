ecd997b57fd3713f171833c94d84fc44eb25562f
Charles Adkins <cpeadkins@gmail.com>
2020-03-12 22:44:22 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,3 @@
-# def my_each(words)
-#   i=0
-#   while i > words.length
-#     my_each(words) do |word|
-#       yield (words[i])
-#       i += 1
-#     end
-#   end
-# end
-
 def my_each(words)
   i = 0
   
